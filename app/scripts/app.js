b72ece833024215d07582588eeef580f3b6857ac
Cara Peer <carapeer@Caras-Mac-mini.attlocal.net>
2016-06-08 09:48:34 -0500

    Refactor with scope, introduceshow/hide details

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,10 +1,12 @@
 'use strict';
 angular.module('confusionApp', [])
 
-        .controller('menuController', function() {
-                        this.tab = 1;
-            this.filtText = '';
-            var dishes=[
+        .controller('MenuController', ['$scope', function($scope) {
+            
+            $scope.tab = 1;
+            $scope.filtText = '';
+            $scope.showDetails = false;
+            $scope.dishes=[
                          {
                           name:'Uthapizza',
                           image: 'images/uthapizza.png',
@@ -42,24 +44,27 @@ angular.module('confusionApp', [])
                            comment: ''
                         }
                         ]; 
-             this.dishes = dishes;
 
-             this.select = function(setTab) {
-                this.tab = setTab;
+             $scope.select = function(setTab) {
+                $scope.tab = setTab;
                 if (setTab === 2) {
-                    this.filtText = "appetizer";
+                    $scope.filtText = "appetizer";
                 }
                 else if (setTab === 3) {
-                    this.filtText = "mains";
+                    $scope.filtText = "mains";
                 }
                 else if (setTab === 4) {
-                    this.filtText = "dessert";
+                    $scope.filtText = "dessert";
                 }
                 else {
-                    this.filtText = "";
+                    $scope.filtText = "";
                 }
             };
-            this.isSelected = function (checkTab) {
-                return (this.tab === checkTab);
+            $scope.isSelected = function (checkTab) {
+                return ($scope.tab === checkTab);
             };
-        });
+            
+            $scope.toggleDetails = function() {
+                $scope.showDetails = !$scope.showDetails;
+            };
+        }]);
